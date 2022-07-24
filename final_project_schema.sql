00c6a882cb393003143d01bcde8708d700c85a76
Blaine Moreland <blaine.l.moreland@gmail.com>
2022-07-23 19:36:51 -0500

    New lines of code, and new data set

--- final_project_schema.sql
+++ final_project_schema.sql
@@ -1,3 +1,4 @@
+-- Main Table
 CREATE TABLE weekly_global_streams (
   rank INT NOT NULL,
   uri VARCHAR NOT NULL,
@@ -8,3 +9,8 @@ CREATE TABLE weekly_global_streams (
   previous_rank INT NOT NULL,
   weeks_on_chart INT NOT NULL,
   streams INT NOT NULL);
+
+  -- New Table
+CREATE TABLE artist_genres (
+artist_names VARCHAR NOT NULL,
+artist_genres VARCHAR NOT NULL);
