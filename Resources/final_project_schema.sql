6a7f9300ea2ee2c015eb8570c460660ecfda8d84
Blaine Moreland <blaine.l.moreland@gmail.com>
2022-07-24 17:01:12 -0500

    Large update
    Final update to source_code. Created resources folder and moved all .csv files to this folder.  Added the full dataset that has both artist and genre included.

--- Resources/final_project_schema.sql
+++ Resources/final_project_schema.sql
@@ -13,4 +13,25 @@ CREATE TABLE weekly_global_streams (
   -- New Table
 CREATE TABLE artist_genres (
 artist_names VARCHAR NOT NULL,
-artist_genres VARCHAR NOT NULL);
+artist_genres VARCHAR);
+
+
+SELECT * FROM artist_genres;
+
+SELECT * FROM weekly_global_streams;
+
+SELECT DISTINCT w.rank,
+w.uri,
+w.artist_names,
+w.track_name,
+w.source,
+w.peak_rank,
+w.previous_rank,
+w.weeks_on_chart,
+w.streams,
+a.artist_genres
+
+FROM weekly_global_streams AS w
+LEFT JOIN artist_genres a
+ON w.artist_names = a.artist_name
+ORDER BY rank
