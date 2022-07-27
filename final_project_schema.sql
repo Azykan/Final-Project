3260cbbc40dc296566bebf2155f9819859144a96
Blaine Moreland <blaine.l.moreland@gmail.com>
2022-07-26 21:58:41 -0500

    Update to data

--- final_project_schema.sql
+++ final_project_schema.sql
@@ -8,7 +8,8 @@ CREATE TABLE weekly_global_streams (
   peak_rank INT NOT NULL,
   previous_rank INT NOT NULL,
   weeks_on_chart INT NOT NULL,
-  streams INT NOT NULL);
+  streams INT NOT NULL,
+  week DATE NOT NULL);
 
   -- Genre Table
 CREATE TABLE artist_genres (
@@ -29,9 +30,10 @@ w.peak_rank,
 w.previous_rank,
 w.weeks_on_chart,
 w.streams,
-a.artist_genres
+a.artist_genres,
+w.week
 
 FROM weekly_global_streams AS w
 LEFT JOIN artist_genres AS a
-ON w.artist_names = a.artist_names
+ON w.artist_names = a.artist_name
 ORDER BY rank;
