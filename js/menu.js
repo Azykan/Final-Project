a20cd84c958f623f734b36881dc0646d0ae28fee
Azykan <97486216+Azykan@users.noreply.github.com>
2022-08-07 14:42:23 -0500

    Create menu.js

---
+++ js/menu.js
@@ -0,0 +1,17 @@
+import 'semantic-ui-css/semantic.min.css'
+
+$('.ui.dropdown')
+  .dropdown({
+    values: [
+      {
+        name: 'Male',
+        value: 'male'
+      },
+      {
+        name     : 'Female',
+        value    : 'female',
+        selected : true
+      }
+    ]
+  })
+;
